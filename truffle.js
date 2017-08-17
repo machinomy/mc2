9d5f865c1dc6d0c9273b1505b1f6b54fba21128c
Sergey Ukustov <sergey@ukstv.me>
2017-08-17 14:29:14 +0300

    Yarn

--- truffle.js
+++ truffle.js
@@ -4,6 +4,13 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      host: "localhost",
+      port: 8545,
+      network_id: 3,
+      from: '0xd5173d09c0567bad2b747cabb54e6bb013077d02',
+      password: process.env.PASSWORD
     }
   }
 };
