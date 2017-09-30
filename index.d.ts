c5048302741573d468b259c38079bd195fe100ba
burtovoy <volgobur@gmail.com>
2017-09-30 17:02:59 +0300

    Rm d.ts

--- index.d.ts
+++
@@ -1,7 +0,0 @@
-import Web3 = require('web3');
-export interface TruffleContract {
-    deployed(): Promise<any>;
-}
-export declare let buildERC20Contract: (address: string, web3: Web3) => Promise<TruffleContract>;
-export declare let buildBrokerContract: (web3: Web3) => TruffleContract;
-export declare let buildBrokerTokenContract: (web3: Web3) => TruffleContract;
