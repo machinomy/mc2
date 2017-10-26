1e2be5ab9aec9c7fa92e8089a71db1136a77137a
Alexandr Burtovoy <volgobur@gmail.com>
2017-10-26 12:52:07 +0300

    Rm files

--- migrations/3_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-var Broker = artifacts.require("./Broker.sol");
-var BrokerToken = artifacts.require("./BrokerToken.sol")
-var ERC20example = artifacts.require("./ERC20example.sol");
-
-module.exports = async function(deployer) {
-  deployer.deploy(Broker);
-  deployer.deploy(BrokerToken);
-  deployer.deploy(ERC20example);  
-};
