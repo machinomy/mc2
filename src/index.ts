aab58314a902983df875747e48a762de0c7e19db
ukstv <sergey.ukustov@machinomy.com>
2018-01-20 23:38:16 -0500

    Remove all the contracts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,8 @@
-const truffleContract = require('truffle-contract')
 const util = require('ethereumjs-util')
 import Web3 = require('web3')
 const abi = require('ethereumjs-abi')
 const BN = require('bn.js')
 import BigNumber from 'bignumber.js'
-import Broker from './Broker'
-import TokenBroker from './TokenBroker'
-
-const ERC20Json = require('../build/contracts/ERC20.json')
-
-export {
-  Broker,
-  TokenBroker
-}
 
 export interface Signature {
   v: number
@@ -38,20 +28,4 @@ export function paymentDigest (channelId: string, value: BigNumber, contractAddr
     [channelId.toString(), new BigNumber(value).toString(), new BN(contractAddress, 16), chainId]
   )
   return util.bufferToHex(digest)
-}
-
-export let buildERC20Contract = (address: string, web3: Web3): Promise<any> => {
-  return new Promise((resolve, reject) => {
-    web3.version.getNetwork((error, result) => {
-      if (error) {
-        return reject(error)
-      }
-      let networks: any = {}
-      networks[result] = { address }
-      Object.assign(ERC20Json, { networks } )
-      const ERC20Contract = truffleContract(ERC20Json)
-      ERC20Contract.setProvider(web3.currentProvider)
-      resolve(ERC20Contract)
-    })
-  })
 }
--- test/support.ts
+++ test/support.ts
@@ -14,32 +14,3 @@ export function getNetwork (web3: Web3): Promise<number> {
     })
   })
 }
-
-export namespace ERC20Example {
-  const Json = require('../build/contracts/ERC20example.json')
-
-  export interface Contract {
-    address: string
-
-    mint (receiver: string, amount: BigNumber|number, opts?: Web3.TxData): Promise<TransactionResult>
-    balanceOf (address: string): Promise<BigNumber>
-    approve (address: string, startChannelValue: BigNumber, opts?: Web3.TxData): Promise<TransactionResult>
-    deposit (address: string, channelId: string, startChannelValue: BigNumber, opts?: Web3.TxData): Promise<TransactionResult>
-  }
-
-  export const deploy = function (provider?: Web3.Provider, opts?: Web3.TxData): Promise<Contract> {
-    let instance = contract(Json)
-    if (provider) {
-      instance.setProvider(provider)
-    }
-    return instance.new(opts)
-  }
-
-  export function deployed (provider?: Web3.Provider): Promise<Contract> {
-    let instance = contract(Json)
-    if (provider) {
-      instance.setProvider(provider)
-    }
-    return instance.deployed()
-  }
-}
