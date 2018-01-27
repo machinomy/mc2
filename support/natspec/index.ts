a5b19458c21b1de3178d1801a94414d05ff123f3
ukstv <sergey.ukustov@machinomy.com>
2018-01-27 10:19:48 -0800

    Use external whitelist for natspec

--- support/natspec/index.ts
+++ support/natspec/index.ts
@@ -15,10 +15,13 @@ async function write (fileName: string, data: string): Promise<void> {
 }
 
 async function main () {
+  let packageJsonFilename = path.resolve(__dirname, '../../package.json')
+  let packageJson = JSON.parse(fs.readFileSync(packageJsonFilename).toString())
+  let whitelist = packageJson.natspec.whitelist
   let config = await sources.currentConfig()
   let fullText = await sources.requiredSources(config)
   let solcOutput = await compiler.doc(fullText)
-  let natSpec = await NatSpec.build(solcOutput, [ 'Broker' ])
+  let natSpec = await NatSpec.build(solcOutput, whitelist)
   let outputFile = path.join(config.build_directory, 'doc', 'natspec.json')
   let asString = JSON.stringify(natSpec, null, 4)
   await write(outputFile, asString)
