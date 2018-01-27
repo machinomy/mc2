14d65add346706584f60691198ce060ebfac66da
ukstv <sergey.ukustov@machinomy.com>
2018-01-26 22:13:44 -0800

    Correct a little bit according to dependencies

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,5 +1,4 @@
 // Truffle injects the following into the global scope
-declare var contract: (name: string, callback: (accounts: Array<string>) => void) => void
 declare var before: any
 declare var beforeEach: any
 declare var describe: any
