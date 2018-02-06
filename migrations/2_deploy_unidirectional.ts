2d18bf93e49f23b8bba656de6c50b532cf5cdd36
ukstv <sergey.ukustov@machinomy.com>
2018-02-06 02:12:15 -0800

    Solidity 0.4.19 was actually used
    And fix some styling

--- migrations/2_deploy_unidirectional.ts
+++ migrations/2_deploy_unidirectional.ts
@@ -3,7 +3,7 @@ import * as Deployer from 'truffle-deployer'
 const ECRecovery = artifacts.require('ECRecovery.sol')
 const Unidirectional = artifacts.require('Unidirectional.sol')
 
-module.exports = function deploy (deployer: Deployer) {
+module.exports = function (deployer: Deployer) {
   return deployer.deploy(ECRecovery).then(() => {
     return deployer.link(ECRecovery, Unidirectional)
   }).then(() => {
