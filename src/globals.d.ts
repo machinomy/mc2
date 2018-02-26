2abbe090ced7ba491c2456bc9fb14c94288154be
ukstv <sergey.ukustov@machinomy.com>
2018-02-26 14:56:54 +0300

    Remove contract function from globals.d.ts
    And fix travis script

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -5,4 +5,3 @@ declare var describe: any
 declare var xdescribe: any
 declare var it: any
 declare var specify: any
-declare function contract (name: string, callback: (accounts: Array<string>) => void): void
