59e3c2716e1593f054963698dade7085a572f277
Andrei Riaskov <ryaskov.work@gmail.com>
2018-03-15 18:45:57 +0300

    Add external and internal modifiers to methods.

--- contracts/LibCommon.sol
+++ contracts/LibCommon.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.19;
 
 library LibCommon {
     function recoveryDigest(bytes32 hash) public pure returns(bytes32) {
--- contracts/Multisig.sol
+++ contracts/Multisig.sol
@@ -1,21 +1,16 @@
 pragma solidity ^0.4.19;
 
-import "zeppelin-solidity/contracts/ECRecovery.sol";
-import "./IRegistry.sol";
-import "./BidirectionalCFLibrary.sol";
 import "./LibMultisig.sol";
-import "./LibCommon.sol";
 
 
-contract MultisigProto {
+contract Multisig {
     LibMultisig.State public state;
 
-
-    function MultisigProto(address _sender, address _receiver)  public {
+    function Multisig(address _sender, address _receiver)  public {
         state = LibMultisig.State(_sender, _receiver, 0);
     }
 
-    function () payable public {}
+    function () payable external {}
 
     function execute(
         address destination,
@@ -23,7 +18,7 @@ contract MultisigProto {
         bytes data,
         bytes senderSig,
         bytes receiverSig
-    ) public
+    ) external
     {
         LibMultisig.execute(destination, value, data, senderSig, receiverSig, state);
         require(destination.call.value(value)(data)); // solium-disable-line security/no-call-value
@@ -35,16 +30,9 @@ contract MultisigProto {
         bytes data,
         bytes senderSig,
         bytes receiverSig
-    ) public
+    ) external
     {
         LibMultisig.executeDelegate(destination, value, data, senderSig, receiverSig, state);
         require(destination.delegatecall(data)); // solium-disable-line security/no-low-level-calls
     }
-}
-
-contract Multisig is MultisigProto {
-
-    function Multisig(address _sender, address _receiver) MultisigProto(_sender, _receiver) public {}
-
-    function () payable public {}
 }
